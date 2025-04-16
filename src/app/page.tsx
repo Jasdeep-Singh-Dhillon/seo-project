ede7c45c1a4f29c65c53c9ceb0835959e0c9cd81
Jasdeep Singh <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2025-04-16 09:09:08 -0400

    Implement Navigation and Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,197 @@
 import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FlaminGO Chargers",
+  description:
+    "Discover Flamingo Charger, the innovative pink charger featured on Shark Tank. From Flamingo pre-charged phone chargers with connectors for all devices stay powered on the go with our cutting-edge solutions. Get your Flamingo phone charger today.",
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="w-full m-auto relative">
+          <Image
+            width={1500}
+            height={586}
+            src={"/images/hero_desktop.png"}
+            alt={"Flock of Flamingo"}
+            className="md:block hidden"
+          />
+          <Image
+            width={1024}
+            height={600}
+            src={"/images/hero_mobile.png"}
+            alt={"Flock of Flamingo"}
+            className="block md:hidden"
+          />
+          <span className="absolute top-8 left-16 text-4xl font-extrabold text-white">
+            Be in Charge!
+          </span>
         </div>
+        <section className="px-6">
+          <h1 className="font-bold text-3xl">
+            The Birth of Flamingo Charger: A Shark Tank Success Story
+          </h1>
+          <div className="my-2">
+            The Flamingo Charger gained national recognition after its
+            appearance on Shark Tank. Entrepreneurs introduced this shark tank
+            charger as a game-changing solution for people who are always on the
+            move. With the increasing demand for portable power sources, the
+            Flamingo Charger stood out due to its pre-charged phone charging
+            technology and sleek, stylish design. The Flamingo phone charger
+            Shark Tank featured quickly captured the attention of consumers,
+            thanks to its reliability, efficiency, and aesthetic appeal.{" "}
+          </div>
+        </section>
+        <section className="px-6">
+          <h2 className="font-bold text-3xl">
+            The Revolutionary Flamingo Charger: A Game-Changer in the Charging
+            Industry
+          </h2>
+          <div className="my-2">
+            In today&apos;s fast-paced world, staying connected is more
+            important than ever. Whether you’re traveling, commuting, or simply
+            out and about, having a reliable phone charger is essential. That’s
+            where the Flamingo Charger comes in. This innovative charging
+            solution, featured on Shark Tank, is making waves in the industry
+            with its unique design, superior performance, and unmatched
+            convenience. If you’re looking for the best pink charger Shark Tank
+            has ever showcased, keep reading to learn why the Flamingo Charger
+            is the ultimate choice for all your charging needs.
+          </div>
+        </section>
+        <section className="p-6">
+          <h2 className="font-bold text-3xl">
+            Why Choose the Flamingo Charger?
+          </h2>
+          <div className="my-2">
+            If you’re tired of dealing with tangled wires, slow charging speeds,
+            and unreliable power banks, the Flamingo Charger is the solution
+            you’ve been waiting for. Here’s why this shark charging station is a
+            must-have:
+          </div>
+
+          <ol>
+            <li>
+              <h3 className="font-bold">1. Pre-Charged Convenience</h3>
+              <div>
+                One of the most innovative features of the Flamingo pre-charged
+                phone chargers is their ready-to-use functionality. Unlike
+                traditional power banks that require hours of charging before
+                use, the Flamingo Charger comes fully charged and ready to go
+                right out of the box. This makes it the perfect emergency power
+                source for those who need a quick battery boost on the fly.
+              </div>
+            </li>
+            <li>
+              <h3 className="font-bold">2. Stylish and Unique Design</h3>
+              <div>
+                The Flamingo Charger isn’t just another charging device; it’s a
+                statement piece. With its sleek pink exterior, it has become the
+                go-to pink charger Shark Tank fans have raved about. Its compact
+                size allows you to slip it into your bag or pocket easily,
+                ensuring you’re always prepared.
+              </div>
+            </li>
+            <li>
+              <h3 className="font-bold">3. Fast and Efficient Charging</h3>
+              <div>
+                Time is of the essence, and no one wants to wait hours for their
+                phone to charge. The Flamingo Charger delivers fast, efficient
+                power to your smartphone, ensuring you get the battery boost you
+                need in a matter of minutes. Whether you’re using it as a backup
+                power source or as your primary charger, this charger Shark Tank
+                introduced is designed for optimal performance.
+              </div>
+            </li>
+            <li>
+              <h3 className="font-bold">
+                4. Eco-Friendly and Disposable Options
+              </h3>
+              <div>
+                With sustainability in mind, the Flamingo Charger offers
+                eco-friendly disposable and reusable models. The disposable
+                version is perfect for one-time use, while the rechargeable
+                option provides long-term value. By opting for a Flamingo phone,
+                you’re choosing a charger that aligns with modern environmental
+                concerns.
+              </div>
+            </li>
+            <li>
+              <h3 className="font-bold">5. Versatile Compatibility</h3>
+              <div>
+                The Flamingo Charger is compatible with a wide range of devices,
+                making it the ideal shark charging station for multiple phone
+                brands. Whether you use an iPhone or an Android device, this
+                charger ensures you stay connected no matter what.
+              </div>
+            </li>
+          </ol>
+        </section>
+        <section className="px-6">
+          <h2 className="font-bold text-3xl">
+            How the Flamingo Charger Stands Out from the Competition
+          </h2>
+          <div className="my-2">
+            The market is saturated with portable chargers, but the Flamingo
+            Charger has managed to set itself apart. Here’s how it compares
+            traditional power banks and other charging solutions:
+            <ul>
+              <li>
+                <span className="font-semibold">No Cables Required</span>–
+                Unlike conventional chargers that require additional cables, the
+                Flamingo Charger is designed for direct plug-in use.
+              </li>
+              <li>
+                <span className="font-semibold">Instant Use</span>– Thanks to
+                its pre-charged technology, there’s no waiting time required.
+              </li>
+              <li>
+                <span className="font-semibold">Ultra-Portable</span>–
+                Lightweight and compact, making it easy to carry wherever you
+                go.
+              </li>
+              <li>
+                <span className="font-semibold">Stylish and Trendy </span>– The
+                pink charger Shark Tank fans love is not only functional but
+                also fashionable.
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="px-6">
+          <h2 className="font-bold text-3xl">
+            Where to Buy the Flamingo Charger
+          </h2>
+          <div className="my-2">
+            Looking to get your hands on a Flamingo phone charger Shark Tank
+            featured? This revolutionary product is available online and in
+            select retail stores. Various e-commerce platforms offer deals on
+            Flamingo pre-charged phone chargers, ensuring you can find the best
+            price for your needs.
+          </div>
+        </section>
+        <section className="px-6">
+          <h2 className="font-bold text-3xl">
+            Final Thoughts: The Future of Charging is Here
+          </h2>
+          <div className="my-2">
+            The Flamingo Charger is more than just a charging device, it’s a
+            lifestyle upgrade. Designed for convenience, efficiency, and style,
+            it has become one of the most sought-after charging solutions on the
+            market. Whether you were introduced to it through Shark Tank or
+            stumbled upon it while searching for a reliable shark charging
+            station, one thing is clear: the Flamingo Charger is here to stay.
+            So, if you’re looking for a charger Shark Tank has endorsed, the
+            Flamingo Charger should be at the top of your list. Say goodbye to
+            low battery anxiety and hello to hassle-free charging with the
+            innovative, stylish, and powerful Flamingo Charger!
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/ui/icons";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Hover Card",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Progress",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Scroll-area",
+    href: "/docs/primitives/scroll-area",
+    description: "Visually or semantically separates content.",
+  },
+  {
+    title: "Tabs",
+    href: "/docs/primitives/tabs",
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+  },
+  {
+    title: "Tooltip",
+    href: "/docs/primitives/tooltip",
+    description:
+      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="p-6">
+            <Image
+              src="flamingo.svg"
+              width={20}
+              height={20}
+              className="h-6 w-6"
+              alt="FlaminGO Charger Icon"
+            />
+            <span className="ms-4 text-lg">FlaminGO Chargers</span>
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <Image
+                      src="flamingo.svg"
+                      width={80}
+                      height={80}
+                      alt="FlaminGO Charger Icon"
+                    />
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      Flamingo Charger
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Pre-charged and rechargeable portable chargers that were
+                      featured on the show Shark Tank.
+                    </p>
+                  </Link>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/products" title="Products">
+                Browse through our products.
+              </ListItem>
+              <ListItem href="/about" title="About Us">
+                Learn more about us and our found Smith Baggett.
+              </ListItem>
+              <ListItem href="/contact" title="Contact Us">
+                Want to reach out? Contact Us.
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+       
+        <NavigationMenuItem>
+          <div>
+            <NavigationMenuLink
+              href="/wholesale"
+              className={navigationMenuTriggerStyle()}
+            >
+              Wholesale
+            </NavigationMenuLink>
+          </div>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ComponentRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
